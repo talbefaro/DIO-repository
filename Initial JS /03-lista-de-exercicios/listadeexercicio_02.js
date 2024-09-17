b66b37accf6be503278358c6f517b32f6827f5c2
Taís Albéfaro <talbefaro@gmail.com>
2024-09-17 16:24:58 -0300

    exercise solution

--- Initial JS /03-lista-de-exercicios/listadeexercicio_02.js
+++ Initial JS /03-lista-de-exercicios/listadeexercicio_02.js
@@ -13,18 +13,19 @@ IMC em adultos Condição:
 - Acima de 40 Obsesidade Grave; */
 
 
-const peso = 68;
+const peso = 60;
 const altura = 1.62 ;
-const imc = peso / (altura * altura)
+const imc = peso / (altura * altura);
+console.log(imc.toFixed(2));
 
 if (imc <18.5) {
-    console.log ('Abaixo do peso')
+    console.log ('Abaixo do peso');
 } else if (imc >= 18.5 && imc <= 25){
-    console.log('Peso normal')
-} else if (imc >25 && imc <=30) {
-console.log('Acima do peso')
-} else if (imc >30 && imc <=40 ) {
-    console.log('Obeso')
+    console.log('Peso normal');
+} else if (imc >=25 && imc <=30) {
+console.log('Acima do peso');
+} else if (imc >=30 && imc <=40 ) {
+    console.log('Obeso');
 } else {
-    console.log('Obesidade grave')
+    console.log('Obesidade grave');
 }
