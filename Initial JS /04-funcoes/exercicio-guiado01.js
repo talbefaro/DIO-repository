c6c6e52edf999ca7ef8fdfed18ffcac1bdb0cfc5
Taís Albéfaro <talbefaro@gmail.com>
2024-09-18 09:28:06 -0300

    practicing functions

---
+++ Initial JS /04-funcoes/exercicio-guiado01.js
@@ -0,0 +1,6 @@
+function escrevaMeuNome(nome){
+    console.log('Meu nome é ' + nome);
+}
+
+escrevaMeuNome('Taís');
+escrevaMeuNome('André')
---
+++ Initial JS /04-funcoes/exercicio-guiado02.js
@@ -0,0 +1,16 @@
+function escrevaMeuNome(nome){
+    console.log('Meu nome é ' + nome);
+}
+
+escrevaMeuNome('Taís');
+escrevaMeuNome('André');
+
+function verificarIdade(idade){
+    if(idade >=18){
+        console.log('Maior');
+    } else {
+        console.log('Menor');
+    }
+}
+
+verificarIdade(12)
