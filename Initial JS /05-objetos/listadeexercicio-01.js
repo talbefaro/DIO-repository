e6ca8cd5db2b8c6dd6aa9c01e81bbb3a7d9b4eb2
Taís Albéfaro <talbefaro@gmail.com>
2024-09-19 15:28:28 -0300

    resolving exercise 01

--- Initial JS /05-objetos/listadeexercicio-01.js
+++ Initial JS /05-objetos/listadeexercicio-01.js
@@ -3,3 +3,26 @@ Os carros possuem uma marca, uma cor e um gasto médio de combustível por Kilô
 Crie um método que dado a quantidade de quilómetros e o preço do combustível nos dê o valor 
 gasto em reais para realizar este percurso.*/
 
+
+
+class Carro {
+    marca;
+    cor;
+    gastoMedioPorKm;
+
+    constructor(marca, cor, gastoMedioPorKm) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKm = gastoMedioPorKm;
+    }
+
+    calcularGastoDePercurso(distanciaEmKm , precoCombustivel){
+        return distanciaEmKm * this.gastoMedioPorKm * precoCombustivel;
+    }
+
+}
+
+const uno = new Carro('fiat' , 'prata' , 1/12);
+console.log(uno.calcularGastoDePercurso(70 , 5));
+const palio = new Carro('fiat' , 'preto' , 1/10);
+console.log(palio.calcularGastoDePercurso(1000 , 6.20));
