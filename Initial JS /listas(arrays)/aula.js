d62e30feebc3c645c7a7f789026839f3abd7d8e8
Taís Albéfaro <talbefaro@gmail.com>
2024-09-20 11:44:55 -0300

    arrays class

---
+++ Initial JS /listas(arrays)/aula.js
@@ -0,0 +1,15 @@
+const notas = [1];
+
+notas.push(10);
+notas.push(10);
+notas.push(10);
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
+}
+
+const media = soma / notas.length;
+console.log(media);
